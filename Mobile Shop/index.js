e9c093a6b4534f8544de9fa192d253aea5d50d4e
DanielBorisov1 <danielborisov305@gmail.com>
2024-10-10 18:15:21 +0300

    Shoping cart

--- Mobile Shop/index.js
+++ Mobile Shop/index.js
@@ -45,40 +45,64 @@ $(document).ready(function () {
     //news phones
 
     $("#new-phones .owl-carousel").owlCarousel({
-        loop:true,
-        nav:false,
-        dots:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
             },
 
-            600:{
-              items: 3
+            600: {
+                items: 3
             },
 
-            1000:{
-                items:5
+            1000: {
+                items: 5
             }
         }
     });
 
     //blog owl carousel
     $("#blogs .owl-carousel").owlCarousel({
-        loop:true,
-        nav:false,
-        dots:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
             },
 
-            600:{
-              items: 3
+            600: {
+                items: 3
             }
 
         }
     });
+
+    //product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    let $input = $(".qty .qty_input");
+    //click on qty up botton
+    $qty_up.click(function () {
+
+        if ($input.val() >= 1 && $input.val() <= 9) {
+            $input.val(function (i, oldval) {
+                return ++oldval;
+            });
+        }
+    });
+
+    $qty_down.click(function () {
+
+        if ($input.val() > 1 && $input.val() <= 10) {
+            $input.val(function (i, oldval) {
+                return --oldval;
+            });
+        }
+    });
+
 
 
 
