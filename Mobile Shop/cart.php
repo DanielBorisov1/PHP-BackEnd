5b1cd069fa97f7339907e9ea2a98ca5609487234
DanielBorisov1 <danielborisov305@gmail.com>
2024-10-11 22:59:22 +0300

    Includes-Done

---
+++ Mobile Shop/cart.php
@@ -0,0 +1,23 @@
+<?php
+include('./includes/header.php');
+?>
+
+
+<?php
+
+
+/* include car-tamplate */
+include('./Template/_cart-tamplate.php');
+/* include car-tamplate */
+
+
+/* include new-phones */
+include('./Template/_new-phones.php');
+/* include new-phones */
+
+?>
+
+
+<?php
+include('./includes/footer.php');
+?>
---
+++ Mobile Shop/product.php
@@ -0,0 +1,29 @@
+<?php
+include('./includes/header.php');
+?>
+
+
+<?php
+
+
+
+/* include products */
+include('./Template/_products.php');
+/* include products */
+
+
+/* include top-sale */
+include('./Template/_top-sale.php');
+/* include top-sale */
+
+
+
+?>
+
+
+
+
+
+<?php
+include('./includes/footer.php');
+?>
