69d8ffad75a2812c788982f204e530a32ec08cd1
DanielBorisov1 <danielborisov305@gmail.com>
2024-10-13 15:20:11 +0300

    Cart

--- Mobile Shop/functions.php
+++ Mobile Shop/functions.php
@@ -5,13 +5,25 @@ require('database/DBController.php');
 //require Product class
 require('database/Product.php');
 
+//require Cart Class
+require('database/Cart.php');
+
 //DBController object
 $db = new DBController();
 
 //Product object
 $product = new Product($db);
+$product_shuffle = $product->getData();
+
 
 //print_r($product->getData());
+
+
+//Cart object
+$Cart = new Cart($db);
+
+
+    
 
 
 
