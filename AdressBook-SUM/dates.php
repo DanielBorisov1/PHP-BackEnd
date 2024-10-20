a695f92289efdbecea15862930c6370f35c79789
DanielBorisov1 <danielborisov305@gmail.com>
2024-10-20 14:02:47 +0300

    dates

--- AdressBook-SUM/dates.php
+++ AdressBook-SUM/dates.php
@@ -1,9 +1,12 @@
 <?php
-
-$format = 'Y.m.d H:i:s';
+/*$format = 'Y.m.d H:i:s';
 $ts = time();
 $ts+=3600;//добави час +
-echo date ($format, $ts);
+echo date ($format, $ts);*/
+
+$result = file('data.txt');
+echo '<pre>'. print_r($result, true). '</pre>';
+
 
 
 
